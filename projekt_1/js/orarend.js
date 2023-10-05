16ef2309e78479c40e5ff339e1f93b8284395ccd
2020b-Papp Zsolt <98013425+b0papzso@users.noreply.github.com>
2023-10-05 19:05:56 +0200

    kész projekt feltöltése

--- projekt_1/js/orarend.js
+++ projekt_1/js/orarend.js
@@ -2,17 +2,16 @@ var mode = 0;
 
 function DarkMode()
 {
-    if(mode == 0)
-    {
-        document.body.style.backgroundColor =  "#4a4848";
+    var gomb = document.getElementById("gomb");
+
+    if (mode == 0) {
+        document.body.style.backgroundColor = "#4a4849";
         document.body.style.backgroundBlendMode = "multiply";
-        document.getElementsById("gomb").src = "js/night-mode.png";
+        gomb.src = "js/night-mode.png";
         mode = 1;
-    }
-    else
-    {
-        document.body.style.backgroundColor =  "transparent";
-        document.getElementById("gomb").src = "js/brightness.png";
+    } else {
+        document.body.style.backgroundColor = "transparent";
+        gomb.src = "js/brightness.png";
         mode = 0;
     }
 }
